6c7b534f819f1c9ad16090b40d9e03e1147dbf30
baham4s <61794844+baham4s@users.noreply.github.com>
2022-01-08 14:08:54 +0100

    première version quizz -- Version commenté -- voir TODO (question.js)

--- function.js
+++ function.js
@@ -1,23 +1,71 @@
-window.addEventListener("load", function() {
-    console.log(data)
+// Compteur de bonne réponse
+let cpt_player=0;
+// Compteur du nombre de question effectué
+let cpt_tot=0;
+// Question de manière aléatoire
+let val = Math.floor(Math.random() * 10);
+let img;
 
-});
-
-var cpt=0;
-
-function compareAnswer() {
-    var rep = document.getElementById("name").value;
-    return valAnswer(rep.localeCompare(data.Facile[0][0].reponse));
+// Affiche si la réponse donné est bonne ou fausse
+function printResponse() {
+    let rep = document.getElementById("reponseInput").value;
+    return strResponse(rep.localeCompare(data.Facile[val].reponse));
 }
 
-function valAnswer(value){
+
+function strResponse(value){
+    document.getElementById("reponseInput").value = ""
     if(value){
-        document.writeln("Réponse fausse");
+        document.getElementById("printRes").innerHTML = "Réponse fausse";
+        document.getElementById("res").innerHTML = "La réponse à "+ data.Facile[val].question + data.Facile[val].reponse;
     }
     else{
-        document.writeln("Réponse Valide");
-        cpt++;
+        document.getElementById("printRes").innerHTML = "Réponse bonne";
+        document.getElementById("res").innerHTML = "";
+        cpt_player++;
     }
-    document.write("Nombre de bonne réponses pour le moment", cpt);
+    printScore(cpt_tot++);
 }
 
+// Affichage du score du joueur
+function printScore(){
+    document.getElementById("cpt").innerHTML = cpt_player + "/" + cpt_tot;
+}
+
+// Affichage des questions
+function printQuestion(mode){
+    if (mode === "Facile"){
+        document.getElementById("question").innerHTML = data.Facile[val].question;
+    }
+    else if (mode === "Moyen"){
+        document.getElementById("question").innerHTML = data.Moyen[val].question;
+    }
+    else {
+        document.getElementById("question").innerHTML = data.Difficile[val].question;
+    }
+}
+
+// Mise a jour de la page après clic sur button validé
+function update(mode){
+    printResponse();
+    val = Math.floor(Math.random() * 10);
+    printQuestion(mode);
+    deleteImage();
+    printImage();
+}
+
+// Affichage d'une image
+function printImage(){
+    img = document.createElement("img");
+    img.src = data.Facile[val].image;
+
+    let div = document.getElementById("image");
+    div.appendChild(img);
+    img.setAttribute("style", "width: 120px;");
+}
+
+// Suppression de l'image
+function deleteImage(){
+    let div = document.getElementById("image");
+    div.removeChild(img);
+}
--- question.js
+++ question.js
@@ -1,216 +1,161 @@
+// TODO : Rajouter 5 question dans question.js pcq valeur en dure dans le JS
 data={
   "Facile": [
     {
-      "0":
-      {
         "question":"12 + 7 = ",
         "reponse":"19",
         "image":"val_image"
-      }
     },
     {
-      "1":
-      {
         "question":"1 + 0 = ",
         "reponse":"1",
         "image":"val_image"
-      }
     },
     {
-      "2":
-      {
         "question":"122 + 3 = ",
         "reponse":"125",
         "image":"val_image"
-      }
     },
     {
-      "3":
-      {
         "question":"34 + 5 = ",
         "reponse":"39",
         "image":"val_image"
-      }
     },
     {
-      "4":
-      {
         "question":"23 + 14 = ",
         "reponse":"37",
         "image":"val_image"
-      }
     },
     {
-      "5":
-      {
         "question":"44 - 28 = ",
         "reponse":"16",
         "image":"val_image"
-      }
     },
     {
-      "6":
-      {
         "question":"59 - 41 = ",
         "reponse":"18",
         "image":"val_image"
-      }
     },
     {
-      "7":
-      {
         "question":"165 - 47 = ",
         "reponse":"118",
         "image":"val_image"
-      }
     },
     {
-      "8":
-      {
         "question":"12 - 2 = ",
         "reponse":"10",
         "image":"val_image"
-      }
     },
     {
-      "9":
-      {
         "question":"89 - 88 = ",
         "reponse":"1",
         "image":"val_image"
-      }
     }
   ],
   "Moyen": [
     {
-      "0":
-      {
         "question":"6 * 6 = ",
         "reponse":"36",
         "image":"val_image"
-      }
     },
     {
-      "1":
-      {
         "question":"3 * 7 = ",
         "reponse":"21",
         "image":"val_image"
-      }
     },
     {
-      "2":
-      {
         "question":"12 * 10 = ",
         "reponse":"120",
         "image":"val_image"
-      }
     },
     {
-      "3":
-      {
         "question":"45 * 0 = ",
         "reponse":"0",
         "image":"val_image"
-      }
     },
     {
-      "4":
-      {
         "question":"9 * 7 = ",
         "reponse":"63",
         "image":"val_image"
-      }
     },
     {
-      "5":
-      {
         "question":"10 / 10 = ",
         "reponse":"1",
         "image":"val_image"
-      }
     },
     {
-      "6":
-      {
         "question":"6 / 3 = ",
         "reponse":"2",
         "image":"val_image"
-      }
     },
     {
-      "7":
-      {
         "question":"12 / 2 = ",
         "reponse":"6",
         "image":"val_image"
-      }
     },
     {
-      "8":
-      {
         "question":"39 / 3 = ",
         "reponse":"13",
         "image":"val_image"
-      }
     },
     {
-      "9":
-      {
+
         "question":"25 / 5 = ",
         "reponse":"5",
         "image":"val_image"
-      }
-    },
-    {
-      "10":
-      {
-        "question":"5 / 1 = ",
-        "reponse":"5",
-        "image":"val_image"
-      }
+
     }
   ],
   "Difficile": [
     {
-      "0":
-      {
         "question":"(5*10) + 4 - 2 = ",
         "reponse":"52",
         "image":"val_image"
-      }
     },
     {
-      "1":
-      {
         "question":"4 + 12 + / 6 + 5 = ",
         "reponse":"11",
         "image":"val_image"
-      }
     },
     {
-      "2":
-      {
         "question":"12 * 2 * 10 = ",
         "reponse":"120",
         "image":"val_image"
-      }
     },
     {
-      "3":
-      {
-        "question":"5 - 3 + 7 * 4 = ",
-        "reponse":"30",
-        "image":"val_image"
-      }
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
     },
     {
-      "4":
-      {
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
+    },
+    {
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
+    },
+    {
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
+    },
+    {
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
+    },
+    {
+      "question":"5 - 3 + 7 * 4 = ",
+      "reponse":"30",
+      "image":"val_image"
+    },
+    {
         "question":"8 * 6 - 6 = ",
         "reponse":"42",
         "image":"val_image"
-      }
     }
   ]
 }
