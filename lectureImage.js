ae5e191182530d1b3c26a421f69aecd2729584d9
Julien_PROUDY <julien.proudy@gmail.com>
2022-01-14 14:33:46 +0100

    ajout jeu lecture d'image

---
+++ lectureImage.js
@@ -0,0 +1,35 @@
+data={
+    "Facile": [
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 1 ?",
+            "reponse":["Explosif","explosif"]
+        },
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 2 ?",
+            "reponse":"Inflammable"
+        },
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 3 ?",
+            "reponse":"Comburant"
+        },
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 4 ?",
+            "reponse":"Gaz sous pression"
+        },
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 5 ?",
+            "reponse":"Corrosif"
+        },
+        {
+            "image":"img/picto.PNG",
+            "question":"Que représente le pictogramme 6 ?",
+            "reponse":"Toxicité"
+        },
+    ]
+}
+    
