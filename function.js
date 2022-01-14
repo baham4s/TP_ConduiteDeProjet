904d17878de67b13174337d181ff6f199f4c7e1a
Dorian <dorianrnd0@gmail.com>
2022-01-14 15:07:35 +0100

    saisit number seulement

--- function.js
+++ function.js
@@ -181,3 +181,12 @@ function stopGame(limite){
         window.location.href = "index.html"
     }
 }
+
+function onlyNumberKey(evt) {
+
+    // Only ASCII character in that range allowed
+    var ASCIICode = (evt.which) ? evt.which : evt.keyCode
+    if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+        return false;
+    return true;
+}
